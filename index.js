3463ed9c2486d3b3d1d9d9bec655ad1cbab03895
Jennifer-h03 <111804801+Jennifer-h03@users.noreply.github.com>
2022-11-28 16:28:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.querySelector("h1").addEventListener("click", function (){
-  var colorRandom = Math.floor(Math.random()*16777215)
+  var colorRandom = Math.floor(Math.random()*16777215).toString(16);
 document.body.style.color = "pink";
 });
